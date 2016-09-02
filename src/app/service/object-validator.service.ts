d6c0b2e66afd0900b2c5eb7c715d63dea18d1237
yaroshenko.g <yaroshenko270@gmail.com>
2016-09-02 17:59:19 +0300

    validator

--- src/app/service/object-validator.service.ts
+++ src/app/service/object-validator.service.ts
@@ -13,103 +13,76 @@ export class ObjectValidatorService {
     let result = [];
     let message = {};
 
-    let attr = {status: 
-                  { 
-                    asd: {asd: 'cds'}
-                  }
-              };
-
-    let con = {
-      status: {
-        presence: true,
-        exclusion: {
-          within: ['OK', 'ERROR:general', 'ERROR:target']
-        },
-        inclusion: {
-          within: ['smile', 'OK', 'asd']
-        },
-        type: {
-          within: ['Object']
-        },
-        deeplyType: {
-          exclusion: {
-            within: ['qwerty']
-          },
-          inclusion: {
-            within: ['bad', 'asd']
-          },
-          type: {
-            within: ['Object']
-          }
-        }
-
-      },
-      date: {
-        presence: false,
-        exclusion: {
-          within: ['asd', 'qwerty']
-        },
-        inclusion: {
-          within: ['bad']
-        },
-        type: {
-          within: ['Array', 'Object']
-        }
+    let attr = {
+      status: { 
+        asd: ['asd']
       }
-
+      // data: ['sdf', 1]
     };
 
-    if (
-      this.isArray(attr.status) || 
-      this.isObject(attr.status) || 
-      this.isBoolean(attr.status) ||
-      this.isString(attr.status) || 
-      this.isNumber(attr.status))
-      {
-      if(this.isDefined(con.status.type)) {
-        message = this.type(attr.status, con.status.type.within);
-        result.push(message);
+    let con = {
+      // data: {
+      //   type: Array,
+      //   arrayValueType: String
+      // },
+      status: {
+        required: true,
+        exclusion: ['OK', 'ERROR:general', 'ERROR:target', 'asd'],
+        inclusion: ['smile', 'OK'],
+        type: 'df'
       }
+    }
+  
+
+    // if(this.isObject(attr.status) && this.isDefined(con.status.deeplyType)) {
+    //   if(this.isDefined(con.status.deeplyType.exclusion)) {
+    //     message = this.exclusion(attr.status.asd, con.status.deeplyType.exclusion.within);
+    //     result.push(message);
+    //   }
+    //   if(this.isDefined(con.status.deeplyType.inclusion)) {
+    //     message = this.inclusion(attr.status.asd, con.status.deeplyType.inclusion.within);
+    //     result.push(message);
+    //   }
+    //   if(this.isDefined(con.status.deeplyType.type)) {   
+    //     message = this.type(attr.status.asd, con.status.deeplyType.type.within);
+    //     result.push(message);
+    //   }
+    // }
+
+    if (
+      (this.isArray(attr.status.asd) || 
+      this.isObject(attr.status.asd) || 
+      this.isBoolean(attr.status.asd) ||
+      this.isString(attr.status.asd) || 
+      this.isNumber(attr.status.asd)) && 
+      this.isDefined(con.status.type)) {
+        // alert(attr.status.asd);
+        // alert(con.status.type.constructor === Array);
+        message = this.type(attr.status.asd, con.status.type);
+        result.push(message);
     } 
 
     // else {
     //   throw new Error(`Unexpected response body: ${attr.status}`); 
     // }
 
-    if(this.isObject(attr.status) && this.isDefined(con.status.deeplyType)) {
-      if(this.isDefined(con.status.deeplyType.exclusion)) {
-        message = this.exclusion(attr.status.asd, con.status.deeplyType.exclusion.within);
-        result.push(message);
-      }
-      if(this.isDefined(con.status.deeplyType.inclusion)) {
-        message = this.inclusion(attr.status.asd, con.status.deeplyType.inclusion.within);
-        result.push(message);
-      }
-      if(this.isDefined(con.status.deeplyType.type)) {   
-        message = this.type(attr.status.asd, con.status.deeplyType.type.within);
-        result.push(message);
-      }
-    }
-
-
-
-    if(con.status.presence == true) {
+    if(con.status.required == true) {
       message = this.presence(attr.status);
       result.push(message);
     }
     if(this.isDefined(con.status.exclusion)) {
-      message = this.exclusion(attr.status, con.status.exclusion.within);
+      message = this.exclusion(attr.status.asd, con.status.exclusion);
       result.push(message);
     }
     if(this.isDefined(con.status.inclusion)) {
-      message = this.inclusion(attr.status, con.status.inclusion.within);
+      message = this.inclusion(attr.status.asd, con.status.inclusion);
       result.push(message);
     }
 
-    if(con.status.presence == true) {
-       message = typeof attr.status;
-      result.push(message);
-    }
+    // if(con.status.presence == true) {
+    //    message = typeof attr.status;
+    //   result.push(message);
+    // }
     return result;
     
   }
@@ -118,48 +91,101 @@ export class ObjectValidatorService {
     // if (this.isEmpty(value)) {
     //   return;
     // }
-    for (let i = 0; i < options.length; i++) {
-      
-      switch (options[i]) {
-        case 'Array': 
-          if (this.isArray(value)){
-            message = { message: `${value} is Array` };
-          } else {
-            message = { message: `${value} is not Array` };
+    alert(options.constructor === String);
+    switch (options.constructor) {
+      case Array:
+        for (let i = 0; i < options.length; i++) {
+          // alert(options[i].constructor == Array);
+          switch(options[i].constructor) {
+            case Array:
+              if (this.isArray(value)){
+                message = { message: `${value} is Array` };
+              } else {
+                message = { message: `${value} is not Array` };
+              }
+            break;
+            case Object:
+              if (this.isObject(value)){
+                message = { message: `${value} is Object` };
+              } else {
+                message = { message: `${value} is not Object` };
+              }
+            break;
+            default:
+              throw new Error(`Unexpected response body: ${value}`);
           }
-          break;
-        case 'Object':
-          if (this.isObject(value)){
-            message = { message: `${value} is Object` };
-          } else {
-            message = { message: `${value} is not Object` };
-          }
-          break;
-        case 'Boolean':
-          if (this.isBoolean(value)){
-            message = { message: `${value} is Boolean` };
-          } else {
-            message = { message: `${value} is not Boolean` };
-          }
-          break;
-        case 'String':
-          if (this.isString(value)){
-            message = { message: `${value} is String` };
-          } else {
-            message = { message: `${value} is not String` };
-          }
-          break;
-        case 'Number':
-          if (this.isNumber(value)){
-            message = { message: `${value} is Number` };
-          } else {
-            message = { message: `${value} is not Number` };
-          }
-          break;
-        default:
-          throw new Error(`Unexpected response body: ${value}`);    
-      }
+        }
+        break;
+      case Object: 
+        if (this.isObject(value)){
+          message = { message: `${value} is Object` };
+        } else {
+          message = { message: `${value} is not Object` };
+        }
+      break;
+      case String:
+        if (this.isString(value)){
+          message = { message: `${value} is String` };
+        } else {
+          message = { message: `${value} is not String` };
+        }
+      break;
+      // case Array:
+      //   if (this.isArray(value)){
+      //     message = { message: `${value} is Array` };
+      //   } else {
+      //     message = { message: `${value} is not Array` };
+      //   }
+      // break;
+      default:
+        throw new Error(`Unexpected response body: ${value}`);    
     }
+    // if (options.constructor === Array) {
+      
+    // } 
+
+    // for (let i = 0; i < options.length; i++) {
+      
+    //   switch (options[i]) {
+    //     case 'Array': 
+    //       if (this.isArray(value)){
+    //         message = { message: `${value} is Array` };
+    //       } else {
+    //         message = { message: `${value} is not Array` };
+    //       }
+    //       break;
+    //     case 'Object':
+    //       if (this.isObject(value)){
+    //         message = { message: `${value} is Object` };
+    //       } else {
+    //         message = { message: `${value} is not Object` };
+    //       }
+    //       break;
+    //     case 'Boolean':
+    //       if (this.isBoolean(value)){
+    //         message = { message: `${value} is Boolean` };
+    //       } else {
+    //         message = { message: `${value} is not Boolean` };
+    //       }
+    //       break;
+    //     case 'String':
+    //       if (this.isString(value)){
+    //         message = { message: `${value} is String` };
+    //       } else {
+    //         message = { message: `${value} is not String` };
+    //       }
+    //       break;
+    //     case 'Number':
+    //       if (this.isNumber(value)){
+    //         message = { message: `${value} is Number` };
+    //       } else {
+    //         message = { message: `${value} is not Number` };
+    //       }
+    //       break;
+    //     default:
+    //       throw new Error(`Unexpected response body: ${value}`);    
+    //   }
+    // }
     return message;
   }
 
@@ -190,7 +216,7 @@ export class ObjectValidatorService {
     }
     options = this.extend(options);
     if(!this.contains(options.within, value)) {
-      message = {message: `${value} may contain`};
+      message = {message: `${value} may contain!`};
       return message;
     }
       message = {message: `is restricted`};
